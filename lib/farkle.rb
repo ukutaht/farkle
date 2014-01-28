991a07793f0a67b2dd9c5a232c67926965a0dbeb
heruku <uku.taht@gmail.com>
2014-01-28 21:24:54 +0200

    require files in the right order

--- lib/farkle.rb
+++ lib/farkle.rb
@@ -1,3 +1,4 @@
-require_relative 'farkle/game.rb'
+require_relative 'farkle/player.rb'
 require_relative 'farkle/standard_scorer.rb'
 require_relative 'farkle/cli_presenter.rb'
+require_relative 'farkle/game.rb'
